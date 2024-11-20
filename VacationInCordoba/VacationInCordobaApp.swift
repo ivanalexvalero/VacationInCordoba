71d48243da9361d0e820d5c329caea83db404518
Ivan Alexander Valero <ivanvalero1985@gmail.com>
2024-11-20 11:49:07 -0300

    first commit

---
+++ VacationInCordoba/VacationInCordobaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInCordobaApp.swift
+//  VacationInCordoba
+//
+//  Created by Ivan Alexander Valero on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VacationInCordobaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
